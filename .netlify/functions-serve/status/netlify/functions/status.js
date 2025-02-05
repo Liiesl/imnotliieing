0ad89cd5300ad7d3f358efab5ce012c57d8a81f7
Liiesl <gobloklo777@gmail.com>
2025-02-05 22:46:38 +0700

    Delete .netlify directory

--- .netlify/functions-serve/status/netlify/functions/status.js
+++
@@ -1,9 +0,0 @@
-// netlify/functions/status.js
-exports.handler = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ status: "idle" })
-    // Replace with actual status logic
-  };
-};
-//# sourceMappingURL=status.js.map
